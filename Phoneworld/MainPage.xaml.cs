dffd355d4f44444b08eba5ce7075b0ca7b14313d
RaffDevs <rveronezdeveloper@gmail.com>
2024-10-22 21:23:47 -0300

    first commit

---
+++ Phoneworld/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace Phoneworld;
+
+public partial class MainPage : ContentPage
+{
+    private string transltedNumber;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCall(object? sender, EventArgs e)
+    {
+        if (await this.DisplayAlert(
+                "Dial a Number",
+                $"Would you like to call {transltedNumber} ?",
+                "Yes",
+                "No")
+           )
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(transltedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid!", "Ok");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Dial Unavaiable", "Api was not avaiable", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to call", $"Unexpected error: {ex.Message}", "Ok");
+            }
+        }
+    }
+
+    private void OnTranslate(object? sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        transltedNumber = PhoneWordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(transltedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + transltedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+}
